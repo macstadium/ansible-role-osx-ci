9f0f4d04dd529162005672e8c7bd30fee89a9ba6
Pedro Pombeiro <github@pedro.pombei.ro>
2020-07-10 18:14:21 +0200

    Install Homebrew with new bash install script
    This gets rid of the deprecation warning from the Ruby script

--- files/homebrew.sh
+++ files/homebrew.sh
@@ -4,6 +4,6 @@ USER=$1
 PASS=$2
 
 echo $PASS | sudo -S su $USER
-echo '' | ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+echo '' | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
 exit $?
